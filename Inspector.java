c87bf1efdd80243b5c29e9fe476ca3c4ce6fc7ab
Alec McAllister <alec@hazardousgiraffe.com>
2019-10-25 17:41:52 -0600

    Added super-class, and interface printing. Changed the methods from returning a string, to just printing out the info themselves.

--- Inspector.java
+++ Inspector.java
@@ -4,27 +4,49 @@ import java.lang.reflect.*;
 public class Inspector {
 
 	public void inspect(Object obj, boolean recursive) {
-		Class c = obj.getClass();
-		inspectClass(c, obj, recursive, 0);
+		println("");
+		inspectClass(obj.getClass(), obj, recursive, 0);
 	}
 
 	private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-		println(getDeclaringClass(c));
+		println(getIndent(depth) + "InspectClass: " + getClassName(c));
+
+		printDeclaringClass(c, recursive, depth);
+
+		printSuperClass(c, recursive, depth);
+
+		printInterface(c, recursive, depth);
 	}
 
-	private String getDeclaringClass(Class c) {
-		String result = "Declaring Class: ";
+	private void printDeclaringClass(Class c, boolean recursive, int depth) {
+		println(getIndent(depth) + " Declaring Class: " + getClassName(c.getDeclaringClass()));
+	}
 
-		if (c == null)
-			result = "null";
+	private void printSuperClass(Class c, boolean recursive, int depth) {
+		println(getIndent(depth) + " Super-Class: " + getClassName(c.getSuperclass()));
 
-		else if (c.getDeclaringClass() == null)
-			result += "None";
+		if (c.getSuperclass() != null && c.getSuperclass() != c.getClass())
+			inspectClass(c.getSuperclass(), null, recursive, depth + 1);
+	}
 
-		else
-			result += c.getDeclaringClass().getSimpleName();
+	private void printInterface(Class c, boolean recursive, int depth) {
+		for (Class i : c.getInterfaces()) {
+			println(getIndent(depth) + " Interface: " + getClassName(i));
+			if (c != i)
+				inspectClass(i, null, recursive, depth + 1);
+		}
+	}
 
-		return result;
+	private String getClassName(Class c) {
+		// May want to change to descriptive name at some point
+		return (c != null) ? c.getSimpleName() : "[N/A]";
+	}
+
+	private String getIndent(int depth) {
+		String indent = "";
+		for (int i = 0; i < depth; i++)
+			indent += "\t";
+		return indent;
 	}
 
 	private void print(Object arg) {
