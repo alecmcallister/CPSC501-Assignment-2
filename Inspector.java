42a1498d1eb5e847c4234a55d748dab64d855a28
Alec McAllister <alec@hazardousgiraffe.com>
2019-10-25 16:14:46 -0600

    Running driver for first time. Nothing special here, just made the .txt files.

--- Inspector.java
+++ Inspector.java
@@ -1,12 +1,17 @@
+import java.lang.*;
+import java.lang.reflect.*;
 
 public class Inspector {
 
-    public void inspect(Object obj, boolean recursive) {
-        Class c = obj.getClass();
-        inspectClass(c, obj, recursive, 0);
-    }
+	public void inspect(Object obj, boolean recursive) {
+		Class c = obj.getClass();
+		inspectClass(c, obj, recursive, 0);
+	}
 
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-    }
+	private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+
+		System.out.println("Nothing happens here yet.");
+
+	}
 
 }
